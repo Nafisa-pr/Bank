c1e490bfe25a12f22b7d9a7689425da02a99ee3f
Nafisa-pr <nafisapires3@gmail.com>
2025-10-23 12:29:58 +0100

    Add project files.

---
+++ Bank/Program.cs
@@ -0,0 +1,18 @@
+﻿using Bankclasslibrary;
+using System.Net.NetworkInformation;
+
+
+Account account = new Account
+
+{
+    AccountHolderName = "Meghan Markle",
+    SortCode = "12-23-23",
+    CardNumber = "1234 4532 2322 1211"
+};
+
+account.DisplayAccountDetails();
+
+Console.WriteLine("\nThank you for banking with Nafisa Bank!");
+
+
+
---
+++ Bankclasslibrary/Bankclasslibrary.cs
@@ -0,0 +1,25 @@
+﻿using Bankclasslibrary;
+using System.Runtime.CompilerServices;
+
+namespace Bankclasslibrary
+{
+    public class Account
+    {
+        public string AccountHolderName { get; set; }
+        public string SortCode { get; set; }
+        public string CardNumber { get; set; }
+
+        public void DisplayAccountDetails()
+        {
+            Console.WriteLine("Account Details");
+            Console.WriteLine($"Account Holder Name: {AccountHolderName}");
+            Console.WriteLine($"Sort Code: {SortCode}");
+            Console.WriteLine($"Card Number: {CardNumber}");
+        }
+        public override string ToString()
+        {
+            return $"Account Holder: {AccountHolderName}, Sort Code: {SortCode}, Card Number: {CardNumber}";
+        }
+    }
+}
+   
