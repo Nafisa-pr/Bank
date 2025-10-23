065fcba915669f5d8c6bdaf5ff5853956d9b3a2f
Nafisa-pr <nafisapires3@gmail.com>
2025-10-23 15:01:35 +0100

    Update

--- Bank/Program.cs
+++ Bank/Program.cs
@@ -1,18 +1,17 @@
 ﻿using Bankclasslibrary;
-using System.Net.NetworkInformation;
 
-
-Account account = new Account
-
-{
-    AccountHolderName = "Meghan Markle",
-    SortCode = "12-23-23",
-    CardNumber = "1234 4532 2322 1211"
-};
-
+// Normal Account
+Account account = new Account("Meghan Markle", "12-23-23", "1234 4532 2322 1211", 1000);
 account.DisplayAccountDetails();
+account.Deposit(500);
+account.Withdraw(200);
+Console.WriteLine(account);
 
-Console.WriteLine("\nThank you for banking with Nafisa Bank!");
+// OverDraftAccount
+OverDraftAccount overdraft = new OverDraftAccount("Harry Windsor", "33-44-55", "1111 2222 3333 4444", 1500);
+overdraft.DisplayAccountDetails();
+overdraft.Withdraw(1600);  // within overdraft limit
+overdraft.Withdraw(100);   // may hit overdraft limit
+Console.WriteLine(overdraft);
 
-
-
+Console.WriteLine("\nThank you for banking with Nafisa Bank!"); 
--- Bankclasslibrary/Bankclasslibrary.cs
+++ Bankclasslibrary/Bankclasslibrary.cs
@@ -1,5 +1,4 @@
-﻿using Bankclasslibrary;
-using System.Runtime.CompilerServices;
+﻿using System;
 
 namespace Bankclasslibrary
 {
@@ -8,6 +7,15 @@ namespace Bankclasslibrary
         public string AccountHolderName { get; set; }
         public string SortCode { get; set; }
         public string CardNumber { get; set; }
+        public double Balance { get; protected set; }
+
+        public Account(string accountHolderName = "Unknown", string sortCode = "00-00-00", string cardNumber = "0000 0000 0000 0000", double balance = 0)
+        {
+            AccountHolderName = accountHolderName;
+            SortCode = sortCode;
+            CardNumber = cardNumber;
+            Balance = balance;
+        }
 
         public void DisplayAccountDetails()
         {
@@ -15,11 +23,42 @@ namespace Bankclasslibrary
             Console.WriteLine($"Account Holder Name: {AccountHolderName}");
             Console.WriteLine($"Sort Code: {SortCode}");
             Console.WriteLine($"Card Number: {CardNumber}");
+            Console.WriteLine($"Balance: {Balance}");
         }
+
+        public virtual void Deposit(double amount)
+        {
+            if (amount > 0)
+            {
+                Balance += amount;
+                Console.WriteLine($"Deposited {amount}. New balance: {Balance}");
+            }
+            else
+            {
+                Console.WriteLine("Deposit amount must be positive.");
+            }
+        }
+
+        public virtual void Withdraw(double amount)
+        {
+            if (amount <= 0)
+            {
+                Console.WriteLine("Withdrawal amount must be positive.");
+            }
+            else if (amount > Balance)
+            {
+                Console.WriteLine("Insufficient funds!");
+            }
+            else
+            {
+                Balance -= amount;
+                Console.WriteLine($"Withdrew {amount}. New balance: {Balance}");
+            }
+        }
+
         public override string ToString()
         {
-            return $"Account Holder: {AccountHolderName}, Sort Code: {SortCode}, Card Number: {CardNumber}";
+            return $"Account Holder: {AccountHolderName}, Sort Code: {SortCode}, Card Number: {CardNumber}, Balance: {Balance}";
         }
     }
 }
-   
